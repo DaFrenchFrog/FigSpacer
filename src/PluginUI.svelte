9123f48d4c27da71ddac425a9a75a114938c1ead
DaFrenchFrog <romain.gle@gmail.com>
2022-06-27 14:19:48 +0200

    first commit

---
+++ src/PluginUI.svelte
@@ -0,0 +1,26 @@
+<script>
+
+	import { Checkbox } from 'figma-plugin-ds-svelte';
+
+    let isChecked = false;
+
+	function toggleSpacers() {
+        console.log("Toggle spacer called..."+isChecked)
+		parent.postMessage({ pluginMessage: { 
+			'type': 'toggle-spacers', 
+			'status': isChecked,
+		} }, '*');
+	}
+</script>
+
+
+<div class="wrapper p-xxsmall">
+<Checkbox on:change={toggleSpacers} bind:checked={isChecked}>Afficher les espacements</Checkbox>
+ </div>
+
+
+<style>
+
+/* Add additional global or scoped styles here */
+
+</style>
---
+++ src/code.ts
@@ -0,0 +1,26 @@
+figma.showUI(__html__, { width: 232, height: 100 });
+
+let isSpacersVisible = false;
+
+figma.on("currentpagechange", () => {
+      console.log("changed  ----> isSpacersVisible = " + isSpacersVisible);
+      updateSpacer();
+});
+
+figma.ui.onmessage = (msg) => {
+      console.log("Toggle spacer called 1...");
+      if (msg.type === "toggle-spacers") {
+            isSpacersVisible = msg.status;
+            updateSpacer();
+      }
+};
+
+function updateSpacer() {
+      var list = figma.currentPage.findAll(
+            (n) => n.name[0] == "⭥" || n.name[0] == "⭤"
+      );
+      list.forEach((obj) => {
+            var o: InstanceNode = <InstanceNode>obj;
+            o.opacity = isSpacersVisible ? 1 : 0;
+      });
+}
